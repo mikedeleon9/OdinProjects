1b21fb60015e8b9ab73b2cf3d8cee7e6505daae4
mikedeleon9 <mikedeleon9@gmail.com>
2024-10-09 15:26:13 -0400

    changing the layout

--- LibraryProject/script.js
+++ LibraryProject/script.js
@@ -16,8 +16,8 @@ function Book(title, author, pages, bookRead){
 
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", "295", "Not Read");
 const theSunAlsoRise = new Book("The Sun Also Rises", "Ernest Hemingway", "289 pages", "Read");
-
-
+const sapiens = new Book("Sapiens: A Brief History of Humankind", "Yuval Noah Harari", "443", "Read")
+const ninetyEightyFour = new Book("Nineteen Eighty-Four", "George Orwell", "323", "Read")
 
 function addBookToLibrary(book) {
  myLibrary.push(book);
@@ -25,18 +25,34 @@ function addBookToLibrary(book) {
 
 addBookToLibrary(theHobbit);
 addBookToLibrary(theSunAlsoRise)
+addBookToLibrary(sapiens);
+addBookToLibrary(ninetyEightyFour)
+
 
 myLibrary.forEach(book => {
     const bookCard = document.createElement('div');
     const newBookButton = document.createElement('button');
+    const modalElement = document.createElement('div');
+    
+
     newBookButton.textContent = "New Book"
+    newBookButton.onclick = createNewEntry();
 
     bookCard.classList.add('book-card');
     bookCard.innerHTML = `
    <div> Title: ${book.title} </div>
    <div> Author:  ${book.author} </div>
    <div> Number of Pages: ${book.pages} </div>
-    `
+    `;
+
+    modalElement.classList.add('modal')
+
+
     mainContainer.appendChild(bookCard);
-    bookCard.appendChild(newBookButton)
+    bookCard.appendChild(newBookButton);
+    bookCard.appendChild(modalElement);
 })
+
+function createNewEntry(){
+ 
+}
