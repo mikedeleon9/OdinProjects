4412c2f6cb95dd415eb0b2d14b9a6903bb3c3500
mikedeleon9 <mikedeleon9@gmail.com>
2024-10-22 16:52:03 -0400

    adding styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"],
+  content: [
+    "./*.{html,js}", // Root directory files
+    "./src/**/*.{html,js}", // Files in the src directory and subdirectories
+    "./components/**/*.{html,js}", // Files in the components directory and subdirectories
+  ],
   theme: {
     extend: {},
   },
